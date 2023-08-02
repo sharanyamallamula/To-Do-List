e904ca9d743d095845d5c9b42fc9845990df9f3f
sharanyamallamula <sharanyamallamula@gmail.com>
2023-08-02 09:39:36 -0400

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by scholar on 8/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+     .padding()
+        
+    } //closing bracket
+} //closing bracket
+
+
+// nothing is edited under here
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by scholar on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
